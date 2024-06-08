835d0f9119dc83d17d7366030018123f59cad567
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-08 20:15:35 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -1,4 +1,4 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 License, learn more at https://ultralytics.com/license
+# Ultralytics YOLO 🚀, AGPL-3.0 https://ultralytics.com/license/
 
 import numpy as np
 from bokeh import io, models, palettes, plotting
