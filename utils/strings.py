4dbe299664517f1fe1aff8d70a15fd92312aa3e7
UltralyticsAssistant <web@ultralytics.com>
2024-06-08 18:21:21 +0000

    Auto-format by https://ultralytics.com/actions

--- utils/strings.py
+++ utils/strings.py
@@ -1,5 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 License https://ultralytics.com/license
 
+
 def filenamesplit(string):  # splits a full filename string into path, file, and extension.
     """Splits a full filename string into path, file, and extension; returns a tuple (path, file, extension,
     fileext).
